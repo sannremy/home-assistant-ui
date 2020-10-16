bf1a28678b007f82acb2a4fdf2baed403e4ecc8b
Sann-Remy Chea <sannremy.chea@gmail.com>
2020-10-16 16:57:16 +0200

    Changed status bar color for iOS

--- pages/index.js
+++ pages/index.js
@@ -102,7 +102,7 @@ class Home extends React.Component {
 
           <meta name="application-name" content="Home Assistant UI" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
           <meta name="apple-mobile-web-app-title" content="Home Assistant UI" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
