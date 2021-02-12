3bf9f623397aa3231ead093af8097ac853326618
Sann-Remy Chea <667617+sannremy@users.noreply.github.com>
2021-02-12 20:38:39 +0100

    Fixed switch

--- components/switch.js
+++ components/switch.js
@@ -5,17 +5,19 @@ class Switch extends React.Component {
     const {
       isActive,
       onChange,
+      id,
+      name,
       children,
     } = this.props
 
     return (
       <div>
         <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
-          <input onChange={onChange} checked={isActive} type="checkbox" name="switch" id="switch" className="Switch absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"/>
-          <label for="switch" className="Switch__label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+          <input onChange={onChange} checked={isActive} type="checkbox" name={name} id={id} className="Switch absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"/>
+          <label htmlFor={id} className="Switch__label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
         </div>
         {children && (
-          <label for="switch">
+          <label htmlFor={id}>
             {children}
           </label>
         )}
